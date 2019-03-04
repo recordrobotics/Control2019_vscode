528a920c7aabb3fdb6f8dfd10ef6677c08dfabf7
fingoldin <vassilioskaxiras@gmail.com>
2019-03-04 17:15:33 +0200

    fix bugs

--- src/main/java/frc/robot/commands/CenterTape.java
+++ src/main/java/frc/robot/commands/CenterTape.java
@@ -5,13 +5,21 @@ import edu.wpi.first.wpilibj.smartdashboard.*;
 import frc.robot.Robot;
 
 public class CenterTape extends Command {
-  public CenterTape() {
+  private double timeout, start_time;
+  private boolean done;
+
+  public CenterTape(long t) {
+    timeout = t;
+    start_time = 0;
+    done = false;
     requires(Robot.newdrivetrain);
   }
 
   @Override
   protected void initialize() {
     Robot.newdrivetrain.stop();
+    start_time = System.getTimeMillis();
+    done = false;
   }
 
   @Override
@@ -25,13 +33,15 @@ public class CenterTape extends Command {
     
     if(line_error1 >= -1.0) {
       Robot.newdrivetrain.curvatureDrive(-base_speed, -Math.max(-turn_clamp, Math.min(turn_clamp, line_error1 * turn_factor)));
+    } else {
+      done = true;
     }
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return done || (System.getTimeMillis() - start_time > timeout);
   }
 
   // Called once after isFinished returns true
--- src/main/java/frc/robot/subsystems/Lifter.java
+++ src/main/java/frc/robot/subsystems/Lifter.java
@@ -23,7 +23,6 @@ public class Lifter extends PIDSubsystem { // This system extends PIDSubsystem
 	private final static double Rd = 10.0;
 	private final static double tolerance = 0.01;
 	private final static double encoder_conv = -1.0/7000.0;
-	private final double[] auto_positions = {0.0, 0.10471428, 0.971, 1.171, 1.9462857, 2.0957};
 	final static double raiseSpeed = 0.5;
 	final static double lowerSpeed = 0.3;
 	
